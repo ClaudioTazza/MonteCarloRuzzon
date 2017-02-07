779b6afc3b9a7db954707d41bba0df7cdd0956c0
lucifero55 <claudiotazza@gmail.com>
2017-02-07 21:09:00 +0100

    Modifiche

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ int main(){
     puntoFreccia[0] = random_between(-raggio, raggio);
     puntoFreccia[1] = random_between(-raggio, raggio);
 
-    distanza = sqrt((double)(puntoFreccia[0] - puntoFreccia[1]) * (puntoFreccia[0] - puntoFreccia[1]) );
+    distanza = sqrt((puntoFreccia[0] - puntoFreccia[1]) * (puntoFreccia[0] - puntoFreccia[1]) + (0 - 0) * (0 - 0) );
 
     if(distanza < raggio) frecceG++;
   }
